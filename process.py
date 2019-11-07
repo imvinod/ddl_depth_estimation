142a2907bd5e070ab85d53e8fcb6de9217d3673b
pratrajv <prajvanshi3@gatech.edu>
2019-11-07 02:04:14 -0500

    frame filtering changes

--- process.py
+++ process.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import Image
 from src.tests import *
 import time
+from sklearn.decomposition import PCA
 
 
 def RMSE(target, prediction):
@@ -40,6 +41,21 @@ def getDepth(img):
     end = time.time()
     return end-start, depth
 
+def pca(X):
+    pca_imgs = []
+    for i, img in enumerate(X): 
+        pca_img = img.reshape(375, -1 )
+        # Increasing n components will increance explained variance but will decrease our accuracy benefits.
+        pca = PCA(n_components = 64, svd_solver='randomized').fit(pca_img)
+        pca_img = pca.transform(pca_img)
+#         print(pca_img.shape )
+#         print("Retained variance", np.sum(pca.explained_variance_ratio_))
+        img = pca.inverse_transform(pca_img)
+        img = img.reshape(375, 1242, 3)
+        pca_imgs.append(img)
+    return pca_imgs
+
+
 def frameFiltering(X, enabled =True):
     all_preds = []
     durations = []
@@ -65,15 +81,24 @@ def main():
     val_folder = "examples/kitti/val/"
     depths_folder = val_folder + "depths"
     images_folder = val_folder + "images"
-    
+    epsilon = 0.000001
+
     # Load images
     X = load_data_from_folder(images_folder)
     Y = load_data_from_folder(depths_folder, True)
-    
+
+    #print(X.shape)
     avg_duration_with_filtering, preds_with_filtering = frameFiltering(X, True) 
     avg_duration_without_filtering, preds_without_filtering = frameFiltering(X, False)    
-    improvement = round((avg_duration_without_filtering - avg_duration_with_filtering)*100 / avg_duration_without_filtering, 3)
+    improvement = round((avg_duration_without_filtering - avg_duration_with_filtering)*100 / (avg_duration_without_filtering + epsilon), 3)
     print("Percentage improvement in execution time with Frame Filtering: ", improvement)       
+    
+    pca_imgs = pca(X)
+    avg_duration_with_pca, preds = measure_duration(pca_imgs)
+    avg_duration_without_pca, preds = measure_duration(X)
+    improvement = round((avg_duration_without_pca - avg_duration_with_pca)*100 / (avg_duration_without_pca +epsilon), 3)
+    print("Percentage improvement in execution time with PCA: ", improvement)    
+        
     
 if __name__ == '__main__':
     main()
